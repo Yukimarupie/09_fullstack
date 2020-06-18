3380c08199c67c288784ca8c1093c4b1a1e22e58
Yuki Harada <y.harada59@gmail.com>
2020-06-18 22:51:21 +0900

    lec86 1:37 applicationコントローラを弄ってログインしてるuserかどうか判断できるようなbefore_actionを設置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+
+  private
+
+  def current_user
+    return unless session[:user_id]
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+#lecture86 sessionにuser_idの情報がなければreturnする。これはログインしていないことを意味する。
+#もしuser_idが存在しfind_byで該当userがいれば@current_userに格納され、このuserが現在ログイン中のuserであるという判断ができる。
+  
 end
