93224171216bfd604fa1eeccb297b2f0d0635655
Yuki Harada <y.harada59@gmail.com>
2020-06-18 18:01:01 +0900

    till lecture 81 ユーザー認証機能追加の為のモデル・コントローラの作成/ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-
+  get 'mypage' , to: 'users#me'
+  #/mypageのページにアクセスがきた場合は、userコントローラのmeアクションが呼ばれる
+  
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  #loginでは、セッションの作成を意味するのでpost, logoutはセッションの削除を意味するので、delete  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  
+  resources :users, only: %i[new create]
+
   resources :boards
   resources :comments, only: %i[create destroy]
+
 end
 
