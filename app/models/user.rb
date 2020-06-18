dbf3bf704f341e1bfaa21fc6ab4cecc0e5d80c6f
Yuki Harada <y.harada59@gmail.com>
2020-06-18 20:42:15 +0900

    till lecture 82 user名・PASS設定のバリデーション等を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,21 @@
 #  index_users_on_name  (name) UNIQUE
 #
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,
+    presence: true,
+    uniqueness: true.
+    #すでに使用されているuser名は使用できないようにする。
+    length: { maximum: 16 },
+    format: {
+      with: /\A[a-z0-9]+\z/,
+#このwithにマッチしたものでないと許可しないようにする /\Aは行頭 \z/は行末を表す。
+      message: 'は小文字英数字で入力してください'
+    }
+
+  validates :password,
+    length: { minimum: 8 }
+    #今回は入れないが、複雑なPASSを設定してもらいたい場合は、
+    #半角英数字8文字以上100文字以下の正規表現で/\A[a-z\d]{8,100}+\z/iなどを設定したりする。
 end
